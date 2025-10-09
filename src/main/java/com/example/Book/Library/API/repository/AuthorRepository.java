8d4eb2a67921eb096838ebec5f18cad0065cc96a
Iliya Raev <insomniq4@abv.bg>
2025-10-09 19:44:47 +0300

    Create AuthoRepository

--- src/main/java/com/example/Book/Library/API/repository/AuthorRepository.java
+++ src/main/java/com/example/Book/Library/API/repository/AuthorRepository.java
@@ -1,4 +1,8 @@
 package com.example.Book.Library.API.repository;
 
-public interface AuthorRepository {
+
+import com.example.Book.Library.API.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
