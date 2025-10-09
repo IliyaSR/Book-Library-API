3d3723d00ba0c2001ecc1e792ad813e19c56aa09
Iliya Raev <insomniq4@abv.bg>
2025-10-09 19:06:14 +0300

    Create BookController which have two get methods(get all books and get book by id)

---
+++ src/main/java/com/example/Book/Library/API/controller/BookController.java
@@ -0,0 +1,38 @@
+package com.example.Book.Library.API.controller;
+
+import com.example.Book.Library.API.service.BookService;
+import org.hibernate.annotations.NotFound;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> getBooks() {
+        if (bookService.getBooks().isPresent()) {
+            return ResponseEntity.ok(bookService.getBooks());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getBookById(@PathVariable Long id) {
+        if (bookService.getBook(id).isPresent()) {
+            return ResponseEntity.ok(bookService.getBook(id));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+}
