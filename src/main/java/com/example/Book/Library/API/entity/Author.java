b489cb3f4860ce3e3c18dc7ba579794e538303b7
Iliya Raev <insomniq4@abv.bg>
2025-10-09 18:58:16 +0300

    Create Author Entity

---
+++ src/main/java/com/example/Book/Library/API/entity/Author.java
@@ -0,0 +1,33 @@
+package com.example.Book.Library.API.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "This field is required!")
+    @Size(min = 2, max = 60)
+    private String name;
+
+    @Size(max = 60)
+    private String country;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
+}
