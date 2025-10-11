6b750bd6a16bb989c2c48281402f2f42ffb38cc9
Iliya Raev <insomniq4@abv.bg>
2025-10-11 20:36:25 +0300

    Create BookService interface

---
+++ src/main/java/com/example/Book/Library/API/service/BookServiceImpl.java
@@ -0,0 +1,64 @@
+package com.example.Book.Library.API.service;
+
+import com.example.Book.Library.API.dto.AuthorDTO;
+import com.example.Book.Library.API.dto.BookResponseDTO;
+import com.example.Book.Library.API.dto.CategoryDTO;
+import com.example.Book.Library.API.entity.Book;
+import com.example.Book.Library.API.repository.BookRepository;
+import com.example.Book.Library.API.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<BookResponseDTO> getBooks() {
+        List<Book> books = bookRepository.findAll();
+
+        if (books.isEmpty()) {
+            throw new ResourceNotFoundException("No books found");
+        }
+
+        return books.stream()
+                .map(book -> new BookResponseDTO(
+                                book.getTitle(),
+                                book.getDescription(),
+                                book.getPublishYear(),
+                                new AuthorDTO(
+                                        book.getAuthor().getName(),
+                                        book.getAuthor().getCountry()
+                                ),
+                                new CategoryDTO(
+                                        book.getCategory().getName()
+                                )
+                        )
+                ).collect(Collectors.toList());
+    }
+
+    public BookResponseDTO getBook(Long id) {
+        return bookRepository.findById(id)
+                .map(book -> new BookResponseDTO(
+                        book.getTitle(),
+                        book.getDescription(),
+                        book.getPublishYear(),
+                        new AuthorDTO(
+                                book.getAuthor().getName(),
+                                book.getAuthor().getCountry()
+                        ),
+                        new CategoryDTO(
+                                book.getCategory().getName()
+                        )
+                ))
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found with id " + id));
+    }
+}
