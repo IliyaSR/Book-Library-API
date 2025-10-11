3b76737e2cfd30245d877db1b76ac0cf61874113
Iliya Raev <insomniq4@abv.bg>
2025-10-11 20:29:29 +0300

    Create CategoryService interface

---
+++ src/main/java/com/example/Book/Library/API/service/CategoryServiceImpl.java
@@ -0,0 +1,56 @@
+package com.example.Book.Library.API.service;
+
+import com.example.Book.Library.API.dto.CategoryDTO;
+import com.example.Book.Library.API.entity.Book;
+import com.example.Book.Library.API.entity.Category;
+import com.example.Book.Library.API.exception.ResourceNotFoundException;
+import com.example.Book.Library.API.repository.CategoryRepository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CategoryServiceImpl {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        List<Category> categories = categoryRepository.findAll();
+
+        if (categories.isEmpty()) {
+            throw new ResourceNotFoundException("Categories not found");
+        }
+
+        return categories.stream()
+                .map(category -> new CategoryDTO(
+                        category.getName()
+                )).toList();
+    }
+
+    public Category createCategory(CategoryDTO categoryDTO) {
+
+        try {
+            Category category = new Category(
+                    categoryDTO.getName()
+            );
+            return categoryRepository.save(category);
+        } catch (ConstraintViolationException ex) {
+            throw new IllegalArgumentException(ex.getConstraintViolations()
+                    .stream()
+                    .map(ev -> ev.getPropertyPath() + ": " + ev.getMessage())
+                    .collect(Collectors.joining(", "))
+            );
+        }
+
+
+    }
+}
