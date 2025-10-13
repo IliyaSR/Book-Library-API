ef5273144469a0bed8838741e568c04ee8534903
Iliya Raev <insomniq4@abv.bg>
2025-10-13 20:45:49 +0300

    Add new get method

--- src/main/java/com/example/Book/Library/API/service/AuthorService.java
+++ src/main/java/com/example/Book/Library/API/service/AuthorService.java
@@ -3,7 +3,9 @@ package com.example.Book.Library.API.service;
 import com.example.Book.Library.API.dto.AuthorDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AuthorService {
     List<AuthorDTO> getAuthors();
+    Optional<AuthorDTO> getAuthorById(Long id);
 }
