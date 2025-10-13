19e0de02d1b8339010a84d46e3b4751dd808ca50
Iliya Raev <insomniq4@abv.bg>
2025-10-13 20:46:45 +0300

    Implement method from interface AuthorService

--- src/main/java/com/example/Book/Library/API/service/AuthorServiceImpl.java
+++ src/main/java/com/example/Book/Library/API/service/AuthorServiceImpl.java
@@ -1,11 +1,13 @@
 package com.example.Book.Library.API.service;
 
 import com.example.Book.Library.API.dto.AuthorDTO;
+import com.example.Book.Library.API.exception.ResourceNotFoundException;
 import com.example.Book.Library.API.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AuthorServiceImpl implements AuthorService {
@@ -18,11 +20,30 @@ public class AuthorServiceImpl implements AuthorService {
     }
 
     public List<AuthorDTO> getAuthors() {
-        return authorRepository.findAll().stream()
+        List<AuthorDTO> authors = authorRepository.findAll().stream()
                 .map(author -> new AuthorDTO(
                         author.getName(),
                         author.getCountry()
                 )).toList();
 
+        if(authors.isEmpty()){
+            throw new ResourceNotFoundException("No authors found!");
+        }else{
+            return authors;
+        }
+    }
+
+    @Override
+    public Optional<AuthorDTO> getAuthorById(Long id) {
+        Optional<AuthorDTO> authorDTO = authorRepository.findById(id)
+                .map(author -> new AuthorDTO(
+                        author.getName(),
+                        author.getCountry()
+                ));
+        if (authorDTO.isPresent()) {
+            return authorDTO;
+        } else {
+            throw new ResourceNotFoundException("Author not found with id: " + id);
+        }
     }
 }
