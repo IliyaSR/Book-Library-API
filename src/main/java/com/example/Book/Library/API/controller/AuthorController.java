359971e7c8a82bf5917bc08c6d537b6d67623c92
Iliya Raev <insomniq4@abv.bg>
2025-10-13 20:47:31 +0300

    Add GET author method by id

--- src/main/java/com/example/Book/Library/API/controller/AuthorController.java
+++ src/main/java/com/example/Book/Library/API/controller/AuthorController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +22,11 @@ public class AuthorController {
 
     @GetMapping("")
     public ResponseEntity<?> getAuthors() {
-        if (!authorService.getAuthors().isEmpty()){
-            return ResponseEntity.ok(authorService.getAuthors());
-        }else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+        return ResponseEntity.ok(authorService.getAuthors());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> getAuthorById(@PathVariable Long id) {
+        return ResponseEntity.ok(authorService.getAuthorById(id));
     }
 }
