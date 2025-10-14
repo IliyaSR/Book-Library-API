d2ae3f8581da86e69fcb0720af15a8b8e3038ff5
Iliya Raev <insomniq4@abv.bg>
2025-10-14 12:48:59 +0300

    Add new createAuthor method in interface

--- src/main/java/com/example/Book/Library/API/service/AuthorService.java
+++ src/main/java/com/example/Book/Library/API/service/AuthorService.java
@@ -1,6 +1,7 @@
 package com.example.Book.Library.API.service;
 
 import com.example.Book.Library.API.dto.AuthorDTO;
+import com.example.Book.Library.API.entity.Author;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface AuthorService {
     List<AuthorDTO> getAuthors();
     Optional<AuthorDTO> getAuthorById(Long id);
+    Author createAuthor(AuthorDTO authorDTO);
 }
