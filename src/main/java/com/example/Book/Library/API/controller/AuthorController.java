075f979b3f3f3534a51f888c2390d07306daeb02
Iliya Raev <insomniq4@abv.bg>
2025-10-14 12:50:27 +0300

    Add post method in controller createAuthor

--- src/main/java/com/example/Book/Library/API/controller/AuthorController.java
+++ src/main/java/com/example/Book/Library/API/controller/AuthorController.java
@@ -1,13 +1,11 @@
 package com.example.Book.Library.API.controller;
 
+import com.example.Book.Library.API.dto.AuthorDTO;
 import com.example.Book.Library.API.service.AuthorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/authors")
@@ -28,5 +26,10 @@ public class AuthorController {
     @GetMapping("{id}")
     public ResponseEntity<?> getAuthorById(@PathVariable Long id) {
         return ResponseEntity.ok(authorService.getAuthorById(id));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> createAuthor(@RequestBody AuthorDTO authorDTO) {
+        return ResponseEntity.ok(authorService.createAuthor(authorDTO));
     }
 }
