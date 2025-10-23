cae69706563038953b5b7ab7c186593b4aeef7d8
Iliya Raev <insomniq4@abv.bg>
2025-10-23 19:21:25 +0300

    Add MethodArgumentNotValidException class in GlobalExceptionHandler to handle it

--- src/main/java/com/example/Book/Library/API/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/Book/Library/API/exception/GlobalExceptionHandler.java
@@ -3,8 +3,11 @@ package com.example.Book.Library.API.exception;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,7 +23,13 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<String> handleData(DataIntegrityViolationException ex){
+    public ResponseEntity<String> handleData(DataIntegrityViolationException ex) {
         return new ResponseEntity<>("Cannot delete category because books are assigned to it.", HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleBadRequest(MethodArgumentNotValidException ex) {
+        System.out.println(Arrays.toString(ex.getDetailMessageArguments()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Arrays.stream(ex.getDetailMessageArguments()).toList().get(1).toString());
     }
 }
