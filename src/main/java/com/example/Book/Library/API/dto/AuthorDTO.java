e29c40fd09f7aaf67654b032d987212448503100
Iliya Raev <insomniq4@abv.bg>
2025-10-23 19:18:41 +0300

    Add Validations to name and countru fields

--- src/main/java/com/example/Book/Library/API/dto/AuthorDTO.java
+++ src/main/java/com/example/Book/Library/API/dto/AuthorDTO.java
@@ -1,6 +1,8 @@
 package com.example.Book.Library.API.dto;
 
 import com.example.Book.Library.API.entity.Book;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,10 @@ import java.util.List;
 @Data
 public class AuthorDTO {
 
+    @NotBlank(message = "This field is required!")
+    @Size(min = 2, max = 60, message = "The field must be between 2 and 60 characters!")
     private String name;
+
+    @Size(max = 60)
     private String country;
 }
