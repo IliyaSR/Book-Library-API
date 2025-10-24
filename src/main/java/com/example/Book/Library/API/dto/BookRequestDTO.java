3aebc28e7ce300a7434ce4da17df31634651bec3
Iliya Raev <insomniq4@abv.bg>
2025-10-24 20:19:15 +0300

    Add BookRequestDTO

--- src/main/java/com/example/Book/Library/API/dto/BookRequestDTO.java
+++ src/main/java/com/example/Book/Library/API/dto/BookRequestDTO.java
@@ -1,4 +1,19 @@
 package com.example.Book.Library.API.dto;
 
+import com.example.Book.Library.API.entity.Author;
+import com.example.Book.Library.API.entity.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookRequestDTO {
+
+    private String title;
+    private String description;
+    private int publishYear;
+    private Long authorId;
+    private Long categoryId;
 }
