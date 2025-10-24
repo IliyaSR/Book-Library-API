261a22b5583a12652f43a11480a4da8ac2f0400b
Iliya Raev <insomniq4@abv.bg>
2025-10-24 20:19:48 +0300

    Add createBook method POST operation

--- src/main/java/com/example/Book/Library/API/controller/BookController.java
+++ src/main/java/com/example/Book/Library/API/controller/BookController.java
@@ -1,5 +1,7 @@
 package com.example.Book.Library.API.controller;
 
+import com.example.Book.Library.API.dto.BookRequestDTO;
+import com.example.Book.Library.API.dto.BookResponseDTO;
 import com.example.Book.Library.API.service.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -24,5 +26,10 @@ public class BookController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getBookById(@PathVariable Long id) {
         return ResponseEntity.ok(bookService.getBook(id));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> createBook(@RequestBody BookRequestDTO bookDTO){
+        return ResponseEntity.ok(bookService.createBook(bookDTO));
     }
 }
